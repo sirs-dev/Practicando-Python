8c07fe1c7f0d40bcb0728c038abb31a4f9d290b0
sirs-dev <srdev.santamaria@gmail.com>
2024-07-12 22:09:18 -0500

    01 - Se agrega archivo para practicar variables

---
+++ CursoPython-MoureDev/01_Variables.py
@@ -0,0 +1,16 @@
+"""
+Este módulo muestra ejemplos básicos de variables y su impresión.
+"""
+
+my_string_variable = "This is a string variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = True
+print(my_bool_variable)
+
+print(my_string_variable, my_int_variable, my_bool_variable)
+
+print(len(my_string_variable))
