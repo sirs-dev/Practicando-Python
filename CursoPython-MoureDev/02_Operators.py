467c0852ac636f31e3bbdf1dae9bc8f80aae870d
sirs-dev <srdev.santamaria@gmail.com>
2024-07-15 21:09:19 -0500

    Se ingresa archivo de operadores

---
+++ CursoPython-MoureDev/02_Operators.py
@@ -0,0 +1,22 @@
+### Aritmetics Operators ###
+print(3 + 4)    # Addition
+print(3 - 4)    # Subtraction
+print(2 * 2)    # Multiplication
+print(2 / 2)    # Division
+print(10 % 4)   # Modulus
+print(10 // 4)  # Floor division
+print(3 ** 4)   # Exponentiation
+
+print("Hola " + "Sergio")
+print("Hola " * 5)
+
+### Assignment Operators
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 > 4)
+print(3 == 4)
+print(3 != 4)
+
