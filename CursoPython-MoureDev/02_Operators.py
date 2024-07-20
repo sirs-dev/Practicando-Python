73b9b713a3b54108cff56b98d44fb4ff7a6fc96f
sirs-dev <srdev.santamaria@gmail.com>
2024-07-20 16:43:34 -0500

    Se actualiza ejercicios de opereaciones

--- CursoPython-MoureDev/02_Operators.py
+++ CursoPython-MoureDev/02_Operators.py
@@ -65,6 +65,12 @@ print(f"El perimetro del rectangulo es: {perimeter}")
 
 
 ## Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+radius = input("Ingresa el radio del circulo")
+area = 3.14 * radius * radius
+circunference = 2 * 3,14 * radius
+
+print(f"El area del circulo es {area} y la circunferencia es {circunference}")
+
 ## Calculate the slope, x-intercept and y-intercept of y = 2x -2
 ## Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
 ## Compare the slopes in tasks 8 and 9.
