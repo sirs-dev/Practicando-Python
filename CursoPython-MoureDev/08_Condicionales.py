adb779a380351a7df8ed6d458d0195ae1cc0fbe8
sirs-dev <srdev.santamaria@gmail.com>
2024-07-28 19:36:26 -0500

    Se agrega archivo de condicionales

---
+++ CursoPython-MoureDev/08_Condicionales.py
@@ -0,0 +1,30 @@
+### Conditions ###
+
+my_condition = True
+
+if my_condition:
+    print("Se cumple la condicion")
+else:
+    print("No se cumple la condicion")
+
+other_condition = 5 * 2
+
+if other_condition == 11:
+    print("Se cumple la condicion")
+else:
+    print("No se cumple la condicion")
+
+other_condition = 10
+
+if other_condition > 5 and other_condition < 20:
+    print("Se cumple la condicion")
+else:
+    print("No se cumple la condicion")
+
+
+if other_condition > 5 and other_condition < 20:
+    print("Se cumple la condicion")
+elif other_condition > 8 and other_condition < 15:
+    print("Se cumple la condicion")
+else:
+    print("No se cumple la condicion")
