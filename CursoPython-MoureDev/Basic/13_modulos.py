c97bc108b25044c5d4449e9d5c6fa820315169f0
sirs-dev <srdev.santamaria@gmail.com>
2024-08-06 19:15:39 -0500

    Se agrega nueva carpeta Basic

---
+++ CursoPython-MoureDev/Basic/13_modulos.py
@@ -0,0 +1,11 @@
+### Modules ###
+from modules import sum_two_values, fullname
+
+sum_two_values(int("4"), int("5"))
+
+fullname("Sergio", "Ruiz")
+
+import math
+
+print(math.pi)
+print(math.pow(2, 8))
